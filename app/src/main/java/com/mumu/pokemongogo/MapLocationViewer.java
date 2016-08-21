b83f725127d6cf78329af5f5bb741a6cf154145d
josh-hsu <alenbos0517@gmail.com>
2016-08-21 23:15:00 +0800

    Add: add Google Map support (Not verified)

---
+++ app/src/main/java/com/mumu/pokemongogo/MapLocationViewer.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 The Josh Tool Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mumu.pokemongogo;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MapLocationViewer extends FragmentActivity
+        implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    private TextView mTapTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_location_viewer);
+
+        mTapTextView = (TextView) findViewById(R.id.tap_text);
+
+        //setUpMapIfNeeded();
+    }
+
+    private void setUpMap() //If the setUpMapIfNeeded(); is needed then...
+    {
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        mTapTextView.setText("tapped, point=" + point);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        mTapTextView.setText("long pressed, point=" + point);
+    }
+}
